99969e29b3bd6d8a7aa183fef688ba0bec042196
thungp@gmail.com <thungp@gmail.com>
2016-08-24 11:16:37 +0900

    added process 3.0 jars locally in project. removed non weather code like blockies, worms, rays etc...

--- src/VerletWeatherWorld.java
+++ src/VerletWeatherWorld.java
@@ -19,6 +19,8 @@ Change Log:
 1. (PT) Migrated weather API code from NarutoGame2Server
 2. (PT) Exposed cloudCoveragePercentage into City class and converted to a percentage.
 3. (PT) Added city name/wind/direction Text overlay.
+4. (PT) Removed references in the menu to the Verlet worms, rays, blockies, spiders and associated classes
+5. (PT) Added exception handling code to the call to external weather data.
  */
 
 public class VerletWeatherWorld extends PApplet {
@@ -55,18 +57,6 @@ public class VerletWeatherWorld extends PApplet {
 	Menu menu;
 	Cage cage;
 
-	int blockCount = 155;
-	VerletBlock[] blockies = new VerletBlock[blockCount];
-
-	int wormCount = 40;
-	VerletWorm[] worms = new VerletWorm[wormCount];
-
-	int rayCount = 35;
-	VerletRay[] rays = new VerletRay[rayCount];
-
-	int spiderCount = 15;
-	VerletSpider[] spiders = new VerletSpider[spiderCount];
-
 	int sphereCount = 100;
 	VerletSphere[] spheres = new VerletSphere[sphereCount];
 	
@@ -98,7 +88,7 @@ public class VerletWeatherWorld extends PApplet {
 		cage = new Cage(new PVector(width*1.075f, height*1.07f, 700));
 
 		String[] labels = {
-				"worms", "rays", "blockies", "spiders", "soup", "spheres", "clouds"
+				 "spheres", "clouds"
 		};
 		int[] states = {
 				//offState, onState, overState, pressState
@@ -112,34 +102,7 @@ public class VerletWeatherWorld extends PApplet {
 		//RoundedRectButton rb = (RoundedRectButton)(menu.buttons[0]);
 		//rb.cornerRadius = 12;
 
-		// blockies
-		for (int i=0; i<blockies.length; i++) {
-			//VerletBlock(PVector sz, float stiffness)
-			float sz = random(20, 55);
-			blockies[i] = new VerletBlock(new PVector(sz, sz, sz), random(.003f, .1f));
-			blockies[i].push(new PVector (random(-10.01f, 10.01f), random(-10.01f, 10.01f), random(-10.01f, 10.01f)));
-		}
-
-		//worms
-		for (int i=0; i<worms.length; i++) {
-			//VerletWorm(float len, int joints, float stiffness, float thickness)
-			worms[i] = new VerletWorm(random(250, 450), PApplet.parseInt(random(35, 65)), .7f, 2.0f);
-			worms[i].push(new PVector (random(-30.01f, 30.01f), random(-30.01f, 30.01f), random(-30.01f, 30.01f)));
-		}
-
-		// rays
-		for (int i=0; i<rays.length; i++) {
-			//VerletRay(int joints, float radius, float stiffness)
-			rays[i] = new VerletRay(38, random(15, 65), random(.03f, .3f));
-			rays[i].push(new PVector (random(-30.01f, 30.01f), random(-30.01f, 30.01f), random(-30.01f, 30.01f)));
-		}
-
-		// spiders
-		for (int i=0; i<spiders.length; i++) {
-			//VerletSpider(int legs, int joints, float radius, float stiffness)
-			spiders[i] = new VerletSpider(PApplet.parseInt(random(6, 16)), PApplet.parseInt(random(6, 16)), random(75, 125), random(.03f, .3f));
-			spiders[i].push(new PVector (random(-100.01f, 100.01f), random(-100.01f, 100.01f), random(-100.01f, 100.01f)));
-		}
+		
 
 		// spheres
 		for (int i=0; i<spheres.length; i++) {
@@ -206,33 +169,13 @@ public class VerletWeatherWorld extends PApplet {
 		cage.display();
 
 
-		if (menu.getSelected() == "blockies") {
-			for (VerletBlock b : blockies) {
-				b.verlet();
-				b.display();
-			}
-		} else if (menu.getSelected() == "spheres") {
+		if (menu.getSelected() == "spheres") {
 			for (VerletSphere s : spheres) {
 				//System.out.println("spheres menu item selected");
 				s.verlet();
 				s.display();
 			}
 
-		} else if (menu.getSelected() == "worms") {
-			for (VerletWorm s : worms) {
-				s.verlet();
-				s.display();
-			}
-		} else if (menu.getSelected() == "rays") {
-			for (VerletRay r : rays) {
-				r.verlet();
-				r.display();
-			}
-		} else if (menu.getSelected() == "spiders") {
-			for (int i=0; i< spiders.length; i++) {
-				spiders[i].verlet();
-				spiders[i].display();
-			}
 		} else if (menu.getSelected() == "clouds"){
 			for(int i = 0; i < clouds.length; i++) {
 				clouds[i].applyForce(wind.div(10));
@@ -242,24 +185,7 @@ public class VerletWeatherWorld extends PApplet {
 				}
 			}
 
-		} else if (menu.getSelected() == "soup") {
-			for (int i=0; i<worms.length*.5f; i++) {
-				worms[i].verlet();
-				worms[i].display();
-			}
-			for (int i=0; i<rays.length*.5f; i++) {
-				rays[i].verlet();
-				rays[i].display();
-			}
-			for (int i=0; i<blockies.length*.5f; i++) {
-				blockies[i].verlet();
-				blockies[i].display();
-			}
-			for (int i=0; i<spiders.length*.5f; i++) {
-				spiders[i].verlet();
-				spiders[i].display();
-			}
-		}
+		} 
 	}
 	class Cage {
 
@@ -583,86 +509,7 @@ public class VerletWeatherWorld extends PApplet {
 		}
 	}
 
-	class VerletBlock extends VerletObj {
 
-		PVector sz;
-
-		VerletBlock() {
-		}
-
-		VerletBlock(PVector sz, float stiffness) {
-			super(stiffness);
-			balls = new VerletBall[8];
-			sphereDetail(3);
-			this.sz = sz;
-
-			// top balls
-			balls[0] = new VerletBall(new PVector(-sz.x/2, -sz.y/2, sz.z/2)); //LF
-			balls[1] = new VerletBall(new PVector(sz.x/2, -sz.y/2, sz.z/2)); //RF
-			balls[2] = new VerletBall(new PVector(sz.x/2, -sz.y/2, -sz.z/2)); //RB
-			balls[3] = new VerletBall(new PVector(-sz.x/2, -sz.y/2, -sz.z/2)); //LB
-
-			// bottom balls
-			balls[4] = new VerletBall(new PVector(-sz.x/2, sz.y/2, sz.z/2)); //LF
-			balls[5] = new VerletBall(new PVector(sz.x/2, sz.y/2, sz.z/2)); //RF
-			balls[6] = new VerletBall(new PVector(sz.x/2, sz.y/2, -sz.z/2)); //RB
-			balls[7] = new VerletBall(new PVector(-sz.x/2, sz.y/2, -sz.z/2)); //LB
-
-			// sticks
-			// top
-			sticks.add(new VerletStick(balls[0], balls[1], stiffness, true));
-			sticks.add(new VerletStick(balls[1], balls[2], stiffness, true));
-			sticks.add(new VerletStick(balls[2], balls[3], stiffness, true));
-			sticks.add(new VerletStick(balls[3], balls[0], stiffness, true));
-			//crosses
-			sticks.add(new VerletStick(balls[0], balls[2], stiffness, false));
-
-			//bottom
-			sticks.add(new VerletStick(balls[4], balls[5], stiffness, true));
-			sticks.add(new VerletStick(balls[5], balls[6], stiffness, true));
-			sticks.add(new VerletStick(balls[6], balls[7], stiffness, true));
-			sticks.add(new VerletStick(balls[7], balls[4], stiffness, true));
-			//crosses
-			sticks.add(new VerletStick(balls[4], balls[6], stiffness, false));
-
-			// vertical rails
-			sticks.add(new VerletStick(balls[0], balls[4], stiffness, true));
-			sticks.add(new VerletStick(balls[1], balls[5], stiffness, true));
-			sticks.add(new VerletStick(balls[2], balls[6], stiffness, true));
-			sticks.add(new VerletStick(balls[3], balls[7], stiffness, true));
-
-			// diaginals
-			sticks.add(new VerletStick(balls[0], balls[6], stiffness, false));
-			sticks.add(new VerletStick(balls[1], balls[7], stiffness, false));
-			sticks.add(new VerletStick(balls[2], balls[4], stiffness, false));
-			sticks.add(new VerletStick(balls[5], balls[3], stiffness, false));
-
-			// side crosses
-			sticks.add(new VerletStick(balls[3], balls[4], stiffness, false));
-			sticks.add(new VerletStick(balls[1], balls[6], stiffness, false));
-		}
-
-		public void display() {
-
-			noFill();
-			beginShape();
-			for (VerletBall b : balls) {
-				pushMatrix();
-				translate(b.pos.x, b.pos.y, b.pos.z);
-				stroke(255, 75);
-				box(5);
-				popMatrix();
-			}
-
-
-
-			strokeWeight(.75f);
-			stroke(185, 135, 130);
-			for (VerletStick s : sticks) {
-				s.display();
-			}
-		}
-	}
 	abstract class VerletObj {
 
 		VerletBall[] balls;
@@ -724,115 +571,8 @@ public class VerletWeatherWorld extends PApplet {
 			}
 		}
 	}
-	class VerletRay extends VerletObj {
-		int joints;
-		float radius;
 
-		VerletRay() {
-		}
 
-		VerletRay(int joints, float radius, float stiffness) {
-			super(stiffness);
-			this.joints = joints;
-			this.radius = radius;
-			balls = new VerletBall[joints];
-
-			float theta = 0.0f;
-			float randomDepth = random(-200, -50);
-			for (int i=0; i<joints; i++) {
-				balls[i] = new VerletBall(new PVector(cos(theta)*radius, sin(theta)*radius, randomDepth));
-				theta += TWO_PI/joints;
-			}
-
-			for (int i=0; i<joints; i++) {
-				if (i < joints-1) {
-					sticks.add(new VerletStick(balls[i], balls[i+1], stiffness, true));
-				} else {
-					sticks.add(new VerletStick(balls[i], balls[0], stiffness, true));
-				}
-				if (i < joints/2) {
-					sticks.add(new VerletStick(balls[i], balls[i+joints/2], stiffness, true));
-				}
-			}
-		}
-
-		public void display() {
-			//	    fill(105, 75, 105, 15);
-			//	    beginShape();
-			//	    for (VerletBall b : balls) {
-			//	      curveVertex(b.pos.x, b.pos.y, b.pos.z);
-			//	    }
-			//	    endShape(CLOSE);
-
-			strokeWeight(.75f);
-			stroke(195, 155, 195);
-			for (VerletStick s : sticks) {
-				s.display();
-			}
-		}
-	}
-	class VerletSpider extends VerletObj {
-		int legs;
-		int joints;
-		float radius;
-		float bodyRadius = 6;
-		float legGap = 0.0f;
-
-		VerletSpider() {
-		}
-
-		VerletSpider(int legs, int joints, float radius, float stiffness) {
-			super(stiffness);
-			this.legs = legs;
-			this.joints = joints;
-			this.radius = radius;
-			balls = new VerletBall[legs*(joints-1)+1];
-
-			float theta = 0.0f;
-			legGap = radius/joints;
-			// lead node
-			balls[0] = new VerletBall(new PVector(0, 0, 0));
-			for (int i=0, k=0; i<legs; i++) {
-				for (int j=1; j<joints; j++) {
-					k = i*(joints-1)+j;
-					//println(k);
-					balls[k] = new VerletBall(new PVector(bodyRadius+cos(theta)*legGap*j, bodyRadius+sin(theta)*legGap*j, 0));
-					if (j==1) {
-						sticks.add(new VerletStick(balls[0], balls[k], stiffness, true));
-					} else {
-						sticks.add(new VerletStick(balls[k-1], balls[k], stiffness, true));
-					}
-				}
-				theta += TWO_PI/legs;
-			}
-		}
-
-		public void display() {
-			strokeWeight(2.75f);
-			stroke(255, 195, 255);
-			for (VerletStick s : sticks) {
-				s.display();
-			}
-
-			//body
-			strokeWeight(1.75f);
-			pushMatrix();
-			translate(balls[0].pos.x, balls[0].pos.y, balls[0].pos.z);
-			stroke(175, 195, 175);
-			fill(0xffaabbcc);
-			sphere(legGap);
-			popMatrix();
-
-			strokeWeight(1.75f);
-			for (int i=0; i<balls.length; i++) {
-				pushMatrix();
-				translate(balls[i].pos.x, balls[i].pos.y, balls[i].pos.z);
-				fill(125, 135, 190, 25);
-				ellipse(0, 0, 7, 7);
-				popMatrix();
-			}
-		}
-	}
 	class Vec2 {
 		float x, y;
 
